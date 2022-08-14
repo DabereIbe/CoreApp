d5112f3e83ff1ef7b27c80c8d31ff53ec5a6b2b0
DabereIbe <dibeakanma@gmail.com>
2022-08-14 12:40:23 +0100

    Edited Callback Url

--- Utilities/PayStackPayment.cs
+++ Utilities/PayStackPayment.cs
@@ -37,7 +37,7 @@ namespace CoreApp.Utilities
                 Email = Email,
                 Reference = Guid.NewGuid().ToString(),
                 Currency = "NGN",
-                CallbackUrl = "~/Cart/Verify"
+                CallbackUrl = "https://localhost:44392/Cart/Verify" ?? "http://www.watchstop.herokuapp.com/Cart/Verify"
             };
 
             TransactionInitializeResponse response = PayStack.Transactions.Initialize(request);
